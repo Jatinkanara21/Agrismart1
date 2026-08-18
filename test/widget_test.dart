995e401a9e3816e9c07b82f12c2f8c8181adf15b
Jatinkanara21 <jatinkanara2006@gmail.com>
2026-08-18 12:20:54 +0530

    test: fix package import for widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,5 +1,5 @@
+import 'package:agrismart/app.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:agrismart1/main.dart';
 
 void main() {
   testWidgets('AgriSmart app starts successfully', (WidgetTester tester) async {
